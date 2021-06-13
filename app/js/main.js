337ba4719326b041a04a02fe0381ec0e46ae9857
FedorLivshits <fediliv@gmail.com>
2021-06-13 16:55:19 +0300

    added right side modal window

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
-$(function() {
+window.addEventListener('load', () => {
+    function rightSideMenu() {
+        const headerBtn = document.querySelector('.header__btn');
+        const rightSideCloseBtn = document.querySelector('.rightside-menu__close');
+        const rightSideMenu = document.querySelector('.rightside-menu');
 
+        headerBtn.addEventListener('click', () => {
+            rightSideMenu.classList.remove('rightside-menu--close');
+        })
+        rightSideCloseBtn.addEventListener('click', () => {
+            rightSideMenu.classList.add('rightside-menu--close');
+        })
+    }
+
+    rightSideMenu()
 })
